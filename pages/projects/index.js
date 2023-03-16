bceafbb7eb94786d57a0ea5b502fa1575e574fbd
Surya <123004250@sastra.ac.in>
2023-03-16 09:17:08 +0530

    feat: added interactions while hovering sections  and added view live button to projects

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import { getProjectDetails } from "../../services/getProjectsDetails";
 import styles from "../../styles/Projects.module.css";
-import utilStyles from "../../styles/utils.module.css"
-import { AppBar ,NavBar} from "../../components";
+import utilStyles from "../../styles/utils.module.css";
+import { AppBar, NavBar } from "../../components";
 import Link from "next/link";
 export async function getStaticProps() {
   const projectDetails = getProjectDetails();
@@ -15,29 +15,56 @@ export async function getStaticProps() {
 export default function Projects({ projects }) {
   return (
     <div className={styles.page_wrapper}>
-      <AppBar/>
-      <div  className={styles.projects_container}>
-        {projects.map((project) => (
-          <Link href={`/projects/${project.name}`} key={project.name} className={styles.projectWrapper}>
-            <Image
-              src={project.img}
-              alt={project.name}
-              width={270}
-              height={180}
-              className={styles.image}
-            />
-            <div className={styles.details}>
-              <h3>{project.name}</h3>
-              <p className={styles.description}>{project.description}</p>
-              <Link href={`/projects/${project.name}`} className={styles.link_btn}>Learn more</Link>
-            </div>
-          </Link>
-        ))}
+      <AppBar />
+      <div className={styles.projects_container}>
+        {projects.map((project) => {
+          console.log(project);
+          return (
+            <Link
+              href={`/projects/${project.name}`}
+              key={project.name}
+              className={styles.projectWrapper}
+            >
+              <Image
+                src={project.img}
+                alt={project.name}
+                width={270}
+                height={180}
+                className={styles.image}
+              />
+              <div className={styles.details}>
+                <h3>{project.name}</h3>
+                <p className={styles.description}>{project.description}</p>
+                <div className={styles.linksContainer}>
+                  <Link
+                    href={`/projects/${project.name}`}
+                    className={styles.btn_outline}
+                  >
+                    Learn more
+                  </Link>
+                  <a
+                    href={project.websiteUrl}
+                    className={styles.btn_filled}
+                    target="_blank"
+                    rel="noreferrer"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                    }}
+                  >
+                    Live Demo
+                  </a>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
       <div className={styles.sidebar}>
-        <h3 className={`${utilStyles.text_md} ${styles.sidebar_content}`}>{`Projects  (${projects.length})` }</h3>
+        <h3
+          className={`${utilStyles.text_md} ${styles.sidebar_content}`}
+        >{`Projects  (${projects.length})`}</h3>
       </div>
-      <NavBar/>
+      <NavBar />
     </div>
   );
 }
